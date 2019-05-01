532ac05e01b15e77db4f08dcc92a3ad46a42d85a
Eideren <the.eideren@gmail.com>
2019-05-01 10:59:14 +0200

    [iOS] BulletSharp iOS specific build

--- BulletSharp/Native.cs
+++ BulletSharp/Native.cs
@@ -4,7 +4,11 @@ namespace BulletSharp
 {
     public static class Native
     {
+        #if __iOS__
+        public const string Dll = "__Internal";
+        #else
         public const string Dll = "libbulletc";
+        #endif
         public const CallingConvention Conv = CallingConvention.Cdecl;
     }
 }
