3781643f66775a0f0b28f5f18bbf1257f3fe3d54
Eideren <the.eideren@gmail.com>
2019-05-05 14:50:52 +0200

    Incomplete work to produce working clang dll
    See included .txt

--- src/msbuild_winstore.bat
+++ src/msbuild_winstore.bat
@@ -1,15 +1,17 @@
 REM Windows
 mkdir msbuild\x86
 pushd msbuild\x86
-cmake ..\..\..\libbulletc\ -G "Visual Studio 15" -DBUILD_MULTITHREADING=OFF -DBULLET_LIBS_DIR=..\..\..\bullet\Windows\x86\lib
-msbuild libbulletc.sln /p:Configuration=Release
+cmake ..\..\..\libbulletc\ -G "Visual Studio 16" -A Win32 -DBUILD_MULTITHREADING=OFF -DBULLET_LIBS_DIR=..\..\..\bullet\Windows\x86\lib -DCMAKE_CXX_COMPILER:FILEPATH="c:/program files (x86)/microsoft visual studio/2019/preview/common7/ide/commonextensions/microsoft/Llvm/bin/clang-cl.exe" -DCMAKE_C_COMPILER:FILEPATH="c:/program files (x86)/microsoft visual studio/2019/preview/common7/ide/commonextensions/microsoft/Llvm/bin/clang-cl.exe"
+cd /d "C:/Program Files (x86)/Microsoft Visual Studio/2019/Preview/MSBuild/Current/Bin"
+msbuild E:\GitProject\BulletSharpPInvoke\src\msbuild\x86\libbulletc.sln /p:Configuration=Release /p:Toolset=clang_cl_x86 /p:CLToolExe=clang-cl.exe /p:CLToolPath="c:/program files (x86)/microsoft visual studio/2019/preview/common7/ide/commonextensions/microsoft/Llvm/bin/"
 if %ERRORLEVEL% neq 0 GOTO :error_popd
 popd
 
 mkdir msbuild\x64
 pushd msbuild\x64
-cmake ..\..\..\libbulletc\ -G "Visual Studio 15 Win64" -DBUILD_MULTITHREADING=OFF -DBULLET_LIBS_DIR=..\..\..\bullet\Windows\x64\lib
-msbuild libbulletc.sln /p:Configuration=Release
+cmake ..\..\..\libbulletc\ -G "Visual Studio 16" -A x64 -DBUILD_MULTITHREADING=OFF -DBULLET_LIBS_DIR=..\..\..\bullet\Windows\x64\lib -DCMAKE_CXX_COMPILER:FILEPATH="c:/program files (x86)/microsoft visual studio/2019/preview/common7/ide/commonextensions/microsoft/Llvm/bin/clang-cl.exe" -DCMAKE_C_COMPILER:FILEPATH="c:/program files (x86)/microsoft visual studio/2019/preview/common7/ide/commonextensions/microsoft/Llvm/bin/clang-cl.exe"
+cd /d "C:\Program Files (x86)\Microsoft Visual Studio\2019\Preview\MSBuild\Current\Bin"
+msbuild E:\GitProject\BulletSharpPInvoke\src\msbuild\x64\libbulletc.sln /p:Configuration=Release /p:Toolset=clang_cl_x64 /p:CLToolExe=clang-cl.exe /p:CLToolPath="c:/program files (x86)/microsoft visual studio/2019/preview/common7/ide/commonextensions/microsoft/Llvm/bin/"
 if %ERRORLEVEL% neq 0 GOTO :error_popd
 popd
 
